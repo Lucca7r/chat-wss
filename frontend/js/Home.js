64ab36ce938b272b5167549e0734348c07872543
luan_medrado8 <medradoluan6@gmail.com>
2024-04-12 10:28:08 -0300

    indo

--- frontend/js/Home.js
+++ frontend/js/Home.js
@@ -1,23 +1,17 @@
-// Select the chat_geral element
-const chatGeral = document.getElementById("#chat-geral");
+const chatGeral = document.getElementById("chat-geral");
 
 
-chatGeral.addEventListener('click', function() {
-  
-  const chatContent = document.getElementById('conteudo');
-
-
-  if (chatContent.innerHTML === '') {
-   
-    fetch('chat.html')
-      .then(response => response.text())
-      .then(data => {
-   
-        chatContent.innerHTML = data;
+chatGeral.addEventListener('click', function (event) 
+   {
+    event.preventDefault();
+    fetch('chat_geral.html')
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById("conteudo").innerHTML = data;
+        
       })
-      .catch(error => {
-        console.error('Error loading chat content:', error);
-
+      .catch((error) => {
+        console.error("Error:", error);
       });
-  }
-});
+  })
+
