30031a87702fc6acd7b12f1d363678c288c895f8
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-04-16 00:20:52 -0300

    tentei

---
+++ backend/image_process/processamento-img.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');  
+
+const app = express();
+
+app.use(cors());  
+
+__dirname = path.resolve();
+
+app.post('/process', (req, res) => {
+  console.log('Request received');
+  const file = req.files.image;
+  const output = path.join(__dirname, 'output.jpg');
+
+  sharp(file.data)
+    .greyscale()
+    .toFile(output, (err) => {
+      if (err) {
+        res.status(500).send('Image processing failed.');
+      } else {
+        res.sendFile(output);
+      }
+    });
+});
+
+app.listen(5502, () => {
+  console.log('Server is running on port 5502');
+});
+
+app.use(cors({
+  origin: '/process'  
+})); 
