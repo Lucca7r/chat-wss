51d38825937146e92c04b838b845efdaf43f6587
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-04-16 01:36:41 -0300

    api feita

---
+++ frontend/js/api-login.js
@@ -0,0 +1,12 @@
+const photoUser = document.getElementById("photo-user");
+
+      let id = "vFrH132ECKUv6ZMMAlavcCLxGpV5BNWKZ08GaSy8ahg";
+      let url = `https://api.unsplash.com/photos/random?client_id=${id}`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          let img = data.urls.regular;
+          photoUser.innerHTML = `<img id="img_user" src="${img}" alt="user" />`;
+        });
