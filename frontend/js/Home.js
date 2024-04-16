46b90225bf138bbb1c7c3ec847d97f28972cc755
luan_medrado8 <medradoluan6@gmail.com>
2024-04-16 17:10:52 -0300

    "complicado"

--- frontend/js/Home.js
+++ frontend/js/Home.js
@@ -2,6 +2,7 @@ const chatGeral = document.getElementById("chat-geral");
 const chatImage = document.getElementById("chat-image");
 const jogo__memoria = document.getElementById("chat-jogos");
 const nomeSpan = document.getElementById("nome_user");
+const player_musica = document.getElementById("player_musica")
 
 chatGeral.addEventListener('click', function (event) 
    {
@@ -60,6 +61,29 @@ chatImage.addEventListener('click', function (event)
       eval(scripts[i].innerText); // Avalia o conteúdo do script
     }
   }
+
+  player_musica.addEventListener('click', function(event) {
+
+    
+    event.preventDefault();
+    fetch('../player_musica/player_musica.html')
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById("conteudo").innerHTML = data;
+        executeScriptInContent();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+
+function executeScriptInContent() {
+  const scripts = document.getElementById("conteudo").getElementsByTagName("script");
+  for (let i = 0; i < scripts.length; i++) {
+    eval(scripts[i].innerText); // Avalia o conteúdo do script
+  }
+}
+
 
   window.addEventListener("DOMContentLoaded", (event) => {
     var nome = sessionStorage.getItem("nome");
