8499c2e44739ba17ba120c97f8bda06d9c1bb716
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-04-16 22:55:00 -0300

    img random no perfil

--- frontend/js/Home.js
+++ frontend/js/Home.js
@@ -2,69 +2,86 @@ const chatGeral = document.getElementById("chat-geral");
 const chatImage = document.getElementById("chat-image");
 const jogo__memoria = document.getElementById("chat-jogos");
 const nomeSpan = document.getElementById("nome_user");
+const player_musica = document.getElementById("player_musica");
 
-chatGeral.addEventListener('click', function (event) 
-   {
-    event.preventDefault();
-    fetch('chat_geral.html')
-      .then((response) => response.text())
-      .then((data) => {
-        document.getElementById("conteudo").innerHTML = data;
-        executeScriptInContent();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  })
+const photoUser = document.getElementById("testeimg");
 
+let id = "vFrH132ECKUv6ZMMAlavcCLxGpV5BNWKZ08GaSy8ahg";
+let url = `https://api.unsplash.com/photos/random?client_id=${id}&query=space`;
 
-chatImage.addEventListener('click', function (event)
-    {
-      event.preventDefault();
-      fetch('../chat-image/chat_image.html')
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("conteudo").innerHTML = data;
-          executeScriptInContent();
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    });
-
-  function executeScriptInContent() {
-    const scripts = document.getElementById("conteudo").getElementsByTagName("script");
-    for (let i = 0; i < scripts.length; i++) {
-      eval(scripts[i].innerText); // Avalia o conteúdo do script
-    }
-  }
-
-  jogo__memoria.addEventListener('click', function(event) {
-
-    
-      event.preventDefault();
-      fetch('../jogo__memoria/jogo__memoria.html')
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("conteudo").innerHTML = data;
-          executeScriptInContent();
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    });
-
-  function executeScriptInContent() {
-    const scripts = document.getElementById("conteudo").getElementsByTagName("script");
-    for (let i = 0; i < scripts.length; i++) {
-      eval(scripts[i].innerText); // Avalia o conteúdo do script
-    }
-  }
-
-  window.addEventListener("DOMContentLoaded", (event) => {
-    var nome = sessionStorage.getItem("nome");
-
-    nomeSpan.innerHTML = nome;
-
-    console.log(nome);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    let img = data.urls.regular;
+    photoUser.innerHTML = `<img id="img_user" src="${img}" alt="user" />`;
   });
+
+chatGeral.addEventListener("click", function (event) {
+  event.preventDefault();
+  fetch("chat_geral.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("conteudo").innerHTML = data;
+      executeScriptInContent();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+chatImage.addEventListener("click", function (event) {
+  event.preventDefault();
+  fetch("../chat-image/chat_image.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("conteudo").innerHTML = data;
+      executeScriptInContent();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+jogo__memoria.addEventListener("click", function (event) {
+  event.preventDefault();
+  fetch("../jogo__memoria/jogo__memoria.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("conteudo").innerHTML = data;
+      executeScriptInContent();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+/*player_musica.addEventListener("click", function (event) {
+  event.preventDefault();
+  fetch("../player_musica/player_musica.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("conteudo").innerHTML = data;
+      executeScriptInContent();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+*/
+function executeScriptInContent() {
+  const scripts = document
+    .getElementById("conteudo")
+    .getElementsByTagName("script");
+  for (let i = 0; i < scripts.length; i++) {
+    eval(scripts[i].innerText); // Avalia o conteúdo do script
+  }
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  var nome = sessionStorage.getItem("nome");
+
+  nomeSpan.innerHTML = nome;
+
+  console.log(nome);
+});
--- frontend/js/api-login.js
+++ frontend/js/api-login.js
@@ -1,7 +1,7 @@
 const photoUser = document.getElementById("photo-user");
 
       let id = "vFrH132ECKUv6ZMMAlavcCLxGpV5BNWKZ08GaSy8ahg";
-      let url = `https://api.unsplash.com/photos/random?client_id=${id}`;
+      let url = `https://api.unsplash.com/photos/random?client_id=${id}&query=space`;
 
       fetch(url)
         .then((response) => response.json())
