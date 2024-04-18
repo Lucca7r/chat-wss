91d0ee75a69da630f39be771b8fca35ace573d86
Guilherme Braga <guilhermebragariosdacosta@gmail.com>
2024-04-18 15:12:49 -0300

    [main] correçãozinha

--- frontend/js/api-login.js
+++ frontend/js/api-login.js
@@ -2,14 +2,6 @@ const photoUser = document.getElementById("photo-user");
 const username = document.getElementById("floatingInput");
 const checkbox = document.getElementById("flexCheckDefault");
 
-window.onload = function () {
-	var savedName = localStorage.getItem("nome");
-	if (savedName) {
-		username.value = savedName;
-		checkbox.checked = true;
-	}
-};
-
 let id = "vFrH132ECKUv6ZMMAlavcCLxGpV5BNWKZ08GaSy8ahg";
 let url = `https://api.unsplash.com/photos/random?client_id=${id}&query=animals`;
 
@@ -29,6 +21,14 @@ document
 		window.location.href = "/frontend/screen/Home/home-pag.html";
 		console.log(nome);
 	});
+
+window.onload = function () {
+	var savedName = localStorage.getItem("nome");
+	if (savedName) {
+		username.value = savedName;
+		checkbox.checked = true;
+	}
+};
 
 checkbox.addEventListener("change", function () {
 	if (checkbox.checked) {
