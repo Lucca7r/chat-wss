c9c9c01130827e3961adc68a847a91d72079eab4
Lorrana Nasareth <71365592+LorranaNS@users.noreply.github.com>
2024-04-18 23:19:55 +0000

    começo da responsividade

--- frontend/js/Home.js
+++ frontend/js/Home.js
@@ -10,77 +10,87 @@ let id = "vFrH132ECKUv6ZMMAlavcCLxGpV5BNWKZ08GaSy8ahg";
 let url = `https://api.unsplash.com/photos/random?client_id=${id}&query=animals`;
 
 fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    let img = data.urls.regular;
-    photoUser.innerHTML = `<img id="img_user" src="${img}" alt="user" />`;
-  });
+	.then((response) => response.json())
+	.then((data) => {
+		let img = data.urls.regular;
+		photoUser.innerHTML = `<img id="img_user" src="${img}" alt="user" />`;
+	});
 
 chatGeral.addEventListener("click", function (event) {
-  event.preventDefault();
-  fetch("chat_geral.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("conteudo").innerHTML = data;
-      executeScriptInContent();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	event.preventDefault();
+	fetch("chat_geral.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById("conteudo").innerHTML = data;
+			executeScriptInContent();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 });
 
 chatImage.addEventListener("click", function (event) {
-  event.preventDefault();
-  fetch("../chat-image/chat_image.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("conteudo").innerHTML = data;
-      executeScriptInContent();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	event.preventDefault();
+	fetch("../chat-image/chat_image.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById("conteudo").innerHTML = data;
+			executeScriptInContent();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 });
 
 jogo__memoria.addEventListener("click", function (event) {
-  event.preventDefault();
-  fetch("../jogo__memoria/jogo__memoria.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("conteudo").innerHTML = data;
-      executeScriptInContent();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	event.preventDefault();
+	fetch("../jogo__memoria/jogo__memoria.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById("conteudo").innerHTML = data;
+			executeScriptInContent();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 });
 
 player_musica.addEventListener("click", function (event) {
-  event.preventDefault();
-  fetch("../player_musica/player_musica.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("conteudo").innerHTML = data;
-      executeScriptInContent();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	event.preventDefault();
+	fetch("../player_musica/player_musica.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById("conteudo").innerHTML = data;
+			executeScriptInContent();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 });
 
 function executeScriptInContent() {
-  const scripts = document
-    .getElementById("conteudo")
-    .getElementsByTagName("script");
-  for (let i = 0; i < scripts.length; i++) {
-    eval(scripts[i].innerText); // Avalia o conteúdo do script
-  }
+	const scripts = document
+		.getElementById("conteudo")
+		.getElementsByTagName("script");
+	for (let i = 0; i < scripts.length; i++) {
+		eval(scripts[i].innerText); // Avalia o conteúdo do script
+	}
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  var nome = sessionStorage.getItem("nome");
+	var nome = sessionStorage.getItem("nome");
 
-  nomeSpan.innerHTML = nome;
+	nomeSpan.innerHTML = nome;
 
-  console.log(nome);
+	console.log(nome);
+});
+
+document.getElementById('menu-toggle').addEventListener('click', function () {
+	var menu = document.getElementById('menu');
+	if (menu.style.display === 'flex') {
+		menu.style.display = 'none';
+	} else {
+		menu.style.display = 'flex';
+		menu.style.flexDirection = 'column';
+	}
 });
