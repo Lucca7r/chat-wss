a5ee0799cc7cb1d8e01b48ba0c688f0a564072f5
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-04-18 00:03:56 -0300

    new file:   backend/image_process/processamentoimg.js 	new file:   backend/node_modules/@img/sharp-win32-x64/lib/libvips-42.dll 	new file:   backend/node_modules/@img/sharp-win32-x64/lib/libvips-cpp.dll 	new file:   backend/node_modules/@img/sharp-win32-x64/lib/sharp-win32-x64.node

---
+++ backend/image_process/processamentoimg.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const http = require("http");
+const app = express();
+
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+
+__dirname = path.resolve();
+
+app.use(express.static("C:/Users/adm/Documents/chat-wss"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("C:/Users/adm/Documents/chat-wss/login.html"));
+});
+
+app.post("/home-pag", (req, res) => {
+  console.log("Request received");
+  const file = req.files.image;
+  const output = path.join(__dirname, "output.jpg");
+
+  sharp(file.data)
+    .greyscale()
+    .toFile(output, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Image processing failed.");
+      } else {
+        res.sendFile(output);
+      }
+    });
+});
+app.listen(5502, () => {
+  console.log("Server is running on port 5502");
+});
