13a8d56dd81df1461441d777cc4b0921991fe4a6
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-04-17 23:27:45 -0300

    sim eu consegui

--- backend/image_process/processamentoimg.js
+++ backend/image_process/processamentoimg.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const sharp = require('sharp');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');  
-
+const express = require("express");
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const http = require("http");
 const app = express();
 
-app.use(cors());  
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
 __dirname = path.resolve();
 
-app.post('/frontend/screen/Home/home-pag.html', (req, res) => {
+app.use(express.static('C:/Users/adm/Documents/chat-wss'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve('C:/Users/adm/Documents/chat-wss/login.html'));
+});
+
+
+app.post('/home-pag', (req, res) => {
   console.log('Request received');
   const file = req.files.image;
   const output = path.join(__dirname, 'output.jpg');
@@ -19,17 +27,13 @@ app.post('/frontend/screen/Home/home-pag.html', (req, res) => {
     .greyscale()
     .toFile(output, (err) => {
       if (err) {
+        console.error(err);
         res.status(500).send('Image processing failed.');
       } else {
         res.sendFile(output);
       }
     });
 });
-
 app.listen(5502, () => {
-  console.log('Server is running on port 5502');
+  console.log("Server is running on port 5502");
 });
-
-app.use(cors({
-  origin: '/process'  
-})); 
