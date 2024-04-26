0fef059ec4238502b2c895c5cf9937f769809195
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-04-26 20:53:46 -0300

    limpa output

--- backend/image_process/processamentoimg.js
+++ backend/image_process/processamentoimg.js
@@ -11,7 +11,7 @@ app.use(fileUpload());
 __dirname = path.resolve();
 
 app.use(express.static("C:/Users/adm/Documents/chat-wss"));
-app.use('/output', express.static(path.join(__dirname, 'output')));
+app.use("/output", express.static(path.join(__dirname, "output")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("C:/Users/adm/Documents/chat-wss/login.html"));
@@ -21,9 +21,24 @@ const now = require("performance-now");
 
 app.post("/home-pag", (req, res) => {
   console.log("Request received");
-  let files = req.files.images;
   const outputDir = path.join(__dirname, "output");
 
+  // Limpar a pasta de saída
+  fs.readdir(outputDir, (err, files) => {
+    if (err) throw err;
+
+    // Excluir todos os arquivos na pasta de saída
+    for (const file of files) {
+      fs.unlink(path.join(outputDir, file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
+  // Obter os arquivos enviados
+  let files = req.files.images;
+
+  // Contar o tempo de processamento
   let start = now();
 
   // Se apenas um arquivo foi enviado, transforme-o em um array
@@ -31,17 +46,27 @@ app.post("/home-pag", (req, res) => {
     files = [files];
   }
 
+  // Processar cada arquivo
   Promise.all(
     files.map((file, index) => {
+      // Caminho de saída para o arquivo processado
       const output = path.join(outputDir, `output${index}.jpg`);
       return sharp(file.data).greyscale().toFile(output);
     })
-  )
+  ) // Enviar uma resposta após todos os arquivos serem processados
     .then(() => {
       let end = now();
       console.log(`Processing time: ${(end - start).toFixed(3)} ms`);
+
+      // Definir o cabeçalho de cache para evitar o armazenamento em cache
+      res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
+
       // Enviar um array de URLs para as imagens processadas
-      res.json(files.map((_, index) => `http://localhost:5502/output/output${index}.jpg`));
+      res.json(
+        files.map(
+          (_, index) => `http://localhost:5502/output/output${index}.jpg`
+        )
+      );
     })
     .catch((err) => {
       console.error(err);
