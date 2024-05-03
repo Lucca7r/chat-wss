82cb41e735ef6f72695c200c24e51321b8cb3939
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-05-02 23:01:27 -0300

    teste

--- backend/image_process/processamentoimg.js
+++ backend/image_process/processamentoimg.js
@@ -1,90 +1,89 @@
 const express = require("express");
-const sharp = require("sharp");
-const fs = require("fs");
-const path = require("path");
-const app = express();
-
 const fileUpload = require("express-fileupload");
+const path = require("path");
+const fs = require("fs");
+const now = require("performance-now");
+const { Worker } = require("worker_threads");
+
+const app = express();
 app.use(fileUpload());
 
-__dirname = path.resolve();
-
 app.use(express.static("C:/Users/adm/Documents/chat-wss"));
-app.use("/output", express.static(path.join(__dirname, "output")));
+app.use("/output", express.static(path.join(__dirname, "../output")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("C:/Users/adm/Documents/chat-wss/login.html"));
 });
 
-const now = require("performance-now");
-
 app.post("/home-pag", (req, res) => {
   console.log("Request received");
-  const outputDir = path.join(__dirname, "output");
+  const outputDir = path.join(__dirname, "..", "output");
 
-  // Limpar a pasta de saída
-  fs.readdir(outputDir, (err, files) => {
-    if (err) throw err;
+  // Obter os arquivos enviados
+  let files = req.files.images;
 
-    // Excluir todos os arquivos na pasta de saída
-    let deletePromises = files.map(file => {
-      return new Promise((resolve, reject) => {
-        fs.unlink(path.join(outputDir, file), (err) => {
-          if (err) reject(err);
-          resolve();
-        });
+  // Contar o tempo de processamento
+  let start = now();
+
+  // Se apenas um arquivo foi enviado, transforme-o em um array
+  if (!Array.isArray(files)) {
+    files = [files];
+  }
+
+  // Processar cada arquivo
+  let workers = files.map((file, index) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker("./image_process/imageWorker.js", {
+        workerData: {
+          fileData: file.data,
+          output: path.join(outputDir, `output${index}.jpg`),
+        },
+      });
+
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code != 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
       });
     });
-
-    Promise.all(deletePromises)
-      .then(() => {
-        // Obter os arquivos enviados
-        let files = req.files.images;
-
-        // Contar o tempo de processamento
-        let start = now();
-
-        // Se apenas um arquivo foi enviado, transforme-o em um array
-        if (!Array.isArray(files)) {
-          files = [files];
-        }
-
-        // Processar cada arquivo
-        Promise.all(
-          files.map((file, index) => {
-            // Caminho de saída para o arquivo processado
-            const output = path.join(outputDir, `output${index}.jpg`);
-            return sharp(file.data).greyscale().toFile(output);
-          })
-        ) // Enviar uma resposta após todos os arquivos serem processados
-          .then(() => {
-            let end = now();
-            console.log(`Processing time: ${(end - start).toFixed(3)} ms`);
-
-            // Definir o cabeçalho de cache para evitar o armazenamento em cache
-            res.set({
-              "Cache-Control": "no-store, no-cache, must-revalidate, private, Pragma: no-cache, Expires: 0, Cache-Control: max-age=0"
-            });
-
-            // Enviar um array de URLs para as imagens processadas
-            res.json(
-              files.map(
-                (_, index) => `http://localhost:5502/output/output${index}.jpg`
-              )
-            );
-          })
-          .catch((err) => {
-            console.error(err);
-            res.status(500).send("Image processing failed.");
-          });
-      })
-      .catch(err => {
-        // Tratar qualquer erro que possa ter ocorrido durante a exclusão
-        console.error(err);
-      });
   });
+
+  Promise.all(workers)
+    .then(() => {
+      let end = now();
+      console.log(`Processing time: ${(end - start).toFixed(3)} ms`);
+
+      // Definir o cabeçalho de cache para evitar o armazenamento em cache
+      res.set({
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, private, Pragma: no-cache, Expires: 0, Cache-Control: max-age=0",
+      });
+
+      // Enviar uma resposta após todos os arquivos serem processados
+      res.json(files.map((_, index) => `/output/output${index}.jpg`));
+
+      // Limpar a pasta de saída após um certo período de tempo (por exemplo, 5 minutos)
+      setTimeout(() => {
+        fs.readdir(outputDir, (err, files) => {
+          if (err) throw err;
+
+          // Excluir todos os arquivos na pasta de saída
+          for (const file of files) {
+            fs.unlink(path.join(outputDir, file), (err) => {
+              if (err) throw err;
+            });
+          }
+        });
+      }, 5 * 60 * 1000);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      res.status(500).send("An error occurred while processing the images.");
+    });
 });
 
+// Iniciar o servidor
 app.listen(5502, () => {
-  console.log("Server is running on port 5502");
+  console.log("Server started on port 5502");
 });
